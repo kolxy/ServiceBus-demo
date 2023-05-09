64cba1f8f92e66890d372f13fd721810dc7d318b
WHOIS: ldap_open failed <liuma@microsoft.com>
2023-05-08 18:55:21 -0700

    Day 1, service bus walkthrough

---
+++ Demo.Config/Config.cs
@@ -0,0 +1,33 @@
+﻿namespace Demo.Config
+{
+    public class Config
+    {
+        private Dictionary<string, string> env;
+        public Config()
+        {
+            // Bad bad practice, but it's just a demo
+            string root = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
+            var filePath = Path.Combine(root, ".env");
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException();
+
+            env = new Dictionary<string, string>();
+
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                int index = line.IndexOf('=');
+                if (index > 0)
+                {
+                    string key = line.Substring(0, index).Trim();
+                    string value = line.Substring(index + 1).Trim();
+                    env[key] = value;
+                }
+            }
+        }
+
+        public string get(string key)
+        {
+            return env[key];
+        }
+    }
+}
